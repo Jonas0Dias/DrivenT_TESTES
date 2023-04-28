2a388c5d053eee6369d58537ed61c687adb34f07
Jonas0Dias <jonaaasdiaaas@gmail.com>
2023-04-27 21:28:23 -0300

    Ajust avaliador

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -10,17 +10,18 @@ async function findHotels(userId: number): Promise<Hotel[]> {
     const Hotels: Hotel[] = await hotelsRepositoty.findHotels();
     const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
     
-    if (Hotels.length===0 || !enrollment) throw notFoundError();
+    if (!enrollment) throw notFoundError();
+
     const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
-    if (Hotels.length===0 || !ticket) throw notFoundError();
+
+    if (!ticket) throw notFoundError();
     console.log('teste tickets')
 
     const ticketWithType = await ticketsRepository.findTickeWithTypeById(ticket.id)
-    
 
-    if (Hotels.length===0 || !enrollment || !ticket) throw notFoundError();
     if (ticketWithType.TicketType.isRemote || ticket.status !== "PAID" || !ticketWithType.TicketType.includesHotel) throw httpStatus.PAYMENT_REQUIRED
     
+    if (Hotels.length===0 ) throw notFoundError();
     return Hotels;
     
   }
